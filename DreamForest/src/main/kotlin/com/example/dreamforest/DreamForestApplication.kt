d81524ac4bb8af132a2bde721ed8c07382b04967
Pyosangwoo <psw3619@naver.com>
2022-11-27 20:10:39 +0900

    spring+kotlin init setting

---
+++ DreamForest/src/main/kotlin/com/example/dreamforest/DreamForestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dreamforest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DreamForestApplication
+
+fun main(args: Array<String>) {
+    runApplication<DreamForestApplication>(*args)
+}
---
+++ DreamForest/src/test/kotlin/com/example/dreamforest/DreamForestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dreamforest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DreamForestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
